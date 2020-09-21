d30f245eb72784455737061811f6627271118f9b
dipghoshraj <dipghoshraj@gmail.com>
2020-09-21 14:13:05 +0530

    Add new function for all included word search

--- wordlist_first.rb
+++ wordlist_first.rb
@@ -8,11 +8,19 @@ file= "wordlist.txt"
 word = ARGV[0]
 
 def get_words(file, word)
-    Parallel.each(File.open(file,"r").each_line) do |line|
-        if line.start_with?(word)
-          puts "found " + word + " in " + line
-        end
+  Parallel.each(File.open(file,"r").each_line) do |line|
+    if line.start_with?(word)
+      puts "found " + word + " in " + line
     end
+  end
 end 
 
-get_words(file, word)
+def get_included_words(file, word)
+  Parallel.each(File.open(file,"r").each_line) do |line|
+    if line.include?(word)
+      puts "found " + word + " in " + line
+    end
+  end
+end 
+
+# get_words(file, word)
