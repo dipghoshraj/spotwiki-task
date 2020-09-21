d34b420354fe0b80f10813f5d3c7004524f18c18
dipghoshraj <dipghoshraj@gmail.com>
2020-09-21 12:26:00 +0530

    Add single word search ruby fuction

---
+++ wordlist_single.rb
@@ -0,0 +1,19 @@
+#ruby code for the word extraction
+
+
+gem "parallel"
+require 'parallel'
+
+
+file= "wordlist.txt"
+word = "troy"
+
+def get_words(file, word)
+    Parallel.each(File.open(file,"r").each_line) do |line|
+        if line.match word
+          puts "found " + word
+        end
+    end
+end 
+
+get_words(file, word)
