a65558868c0d2fd34aeb460782762713367ac603
dipghoshraj <dipghoshraj@gmail.com>
2020-09-21 14:05:44 +0530

    Fix typo

--- wordlist_first.rb
+++ wordlist_first.rb
@@ -9,8 +9,8 @@ word = ARGV[0]
 
 def get_words(file, word)
     Parallel.each(File.open(file,"r").each_line) do |line|
-        if lines.start_with?(word)
-          puts "found " + word + " in " + lines
+        if line.start_with?(word)
+          puts "found " + word + " in " + line
         end
     end
 end 
