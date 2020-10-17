056b8db6bf60f188f47d1cc01c44b3e32c5189fb
dipghoshraj <dipghoshraj@gmail.com>
2020-10-17 18:57:37 +0530

    Chnage the routing

--- spottask/config/routes.rb
+++ spottask/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  resources :page, only: [:index, :show]
   root to: 'page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'page/show'
 end
