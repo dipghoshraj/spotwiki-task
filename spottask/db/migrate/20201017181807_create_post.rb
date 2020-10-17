1b1c34bee089e58a72fbd8ad913b5864e45177a8
dipghoshraj <dipghoshraj@gmail.com>
2020-10-17 23:51:46 +0530

    Add post migration

--- spottask/db/migrate/20201017181807_create_post.rb
+++ spottask/db/migrate/20201017181807_create_post.rb
@@ -1,6 +1,9 @@
 class CreatePost < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
+      t.string :images
+      t.boolean :active
+      t.string :reference
     end
   end
 end
