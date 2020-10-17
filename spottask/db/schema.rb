72aa0b10619400a4d492d47afc7df50f49d136f4
dipghoshraj <dipghoshraj@gmail.com>
2020-10-17 18:45:19 +0000

    add post model

--- spottask/db/schema.rb
+++ spottask/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_17_152333) do
+ActiveRecord::Schema.define(version: 2020_10_17_181807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "images"
+    t.boolean "active"
+    t.string "reference"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
