5fd70201d545881ab5cd20fcff0982327c1b1ff2
dipghoshraj <dipghoshraj@gmail.com>
2020-10-18 23:05:35 +0530

    create post on route

--- spottask/config/routes.rb
+++ spottask/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :page, only: [:index, :show]
-  resources :account, only: [:index, :show]
+  resources :account, only: [:index, :show, :create]
 
   root to: 'page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
