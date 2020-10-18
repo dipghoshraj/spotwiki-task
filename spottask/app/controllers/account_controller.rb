3a1849e19dd20fbef0165ad4c3df4dfada172c30
dipghoshraj <dipghoshraj@gmail.com>
2020-10-18 22:28:46 +0530

    Added acounts controlller

--- spottask/app/controllers/account_controller.rb
+++ spottask/app/controllers/account_controller.rb
@@ -2,8 +2,20 @@ class AccountController < ApplicationController
     before_action :authenticate_user!
 
     def index
+        @posts = Post.all
     end
 
     def show
+    end
+
+    def create
+        @reference = params[:reference]
+        @images = params[:images]
+        @post = Post.create({:reference => @reference, :images => @images, :active=> true})
+        if @post.errors.blank?
+            render json: { message: 'Posted successfully', user: @user}, status: 200
+        else
+            render json: { message: "cant share this post: #{@user.errors.full_messages.join(',')}"}, status: 403
+        end
     end
 end
